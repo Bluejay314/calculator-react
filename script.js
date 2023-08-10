e51cdc7b275b970b8f15f6d268aa725ec0061a2a
Jarrad <bluejay.jzp@gmail.com>
2023-08-10 10:44:22 +1000

    added script.js

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
 let total = 0;
-let currentValue = null;
-let currentfunc = null;
+let isNegative = false;
+let currentValue = "";
+let currentFunc = null;
 
-document.querySelectorAll("")
+document.querySelectorAll(".number").forEach(e => e.addEventListener("click", numberClickEvent))
+document.querySelectorAll(".operator").forEach(e => e.addEventListener("click", operatorClickEvent))
+
+function updateDisplay(toAppend) {
+    const inputDisplay = document.getElementById("input-display");
+    console.log(inputDisplay.id)
+    inputDisplay.innerText += toAppend;
+}
+
+function numberClickEvent(event) {
+    currentValue += event.target.innerText;
+    updateDisplay(currentValue);
+    logdetails();
+}
+
+function operatorClickEvent(event) {
+    if(currentValue.length != 0 && total != 0)
+        calculate();
+
+    switch(event.target.innerText) {
+        case "+":
+            if(currentValue.length != 0 && !currentFunc) {
+                currentFunc = (a, b) => a + b;
+                total = currentValue;
+                updateDisplay(" + ");
+            }
+            break;
+        case "-":
+            if(currentValue.length == 0 && !isNegative) {
+                isNegative = true;
+                updateDisplay("-");
+            }
+            else {
+                currentFunc = (a, b) => a - b;
+                total = currentValue;
+                updateDisplay(" - ");
+            }
+            break;
+    }
+    logdetails();
+}
+
+function calculate() {
+    total = currentFunc(total, currentValue);
+    currentValue = "";
+    currentFunc = null;
+    logdetails();
+}
+
+function logdetails() {
+    console.log(`total: ${total}`)
+    console.log(`currentValue: ${currentValue}`)
+    console.log(`currentFunc: ${currentFunc}`)
+}
+

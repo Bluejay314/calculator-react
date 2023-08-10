68e7dd7b80162332e3c7a1826a17a490aaea50da
Jarrad <bluejay.jzp@gmail.com>
2023-08-10 14:55:33 +1000

    calculator prints ERROR if passed invalid input

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let total = 0;
 let currentValue = "";
 let currentFunc = null;
 let isNegative = false;
+let willError = false;
 const inputDisplay = document.getElementById("input-display");
 
 document.querySelectorAll(".number").forEach(e => e.addEventListener("click", numberClickEvent))
@@ -16,52 +17,42 @@ function numberClickEvent(event) {
 }
 
 function operatorClickEvent(event) {
+    let operator = event.target.innerText;
     switch(event.target.innerText) {
-        case "+":
-            if(currentValue.length != 0 && !currentFunc) {
-                currentFunc = (a, b) => a + b;
-                total = currentValue;
-                currentValue = "";
-                inputDisplay.innerText += (" +");
-            }
-            break;
+        case "+": setFunc((a, b) => a + b, operator); break;
         case "-":
             if(currentValue.length == 0 && !isNegative)
                 isNegative = true;
             
-            else {
-                currentFunc = (a, b) => a - b;
-                total = currentValue;
-                currentValue = "";
-            }
+            setFunc((a, b) => a - b, operator); break;
 
-            inputDisplay.innerText += (" -");
-            break;
-        case "*":
-            if(currentValue.length != 0 && !currentFunc) {
-                currentFunc = (a, b) => a * b;
-                total = currentValue;
-                currentValue = "";
-                inputDisplay.innerText += (" x");
-            }
-            break;
-        case "/":
-            if(currentValue.length != 0 && !currentFunc) {
-                currentFunc = (a, b) => a / b;
-                total = currentValue;
-                currentValue = "";
-                inputDisplay.innerText += (" /");
-            }
-            break;
+        case "*": setFunc((a, b) => a * b, operator); break;
+        case "/": setFunc((a, b) => a / b, operator); break;
     }
     logdetails();
+
+    function setFunc(func, symbol) {
+        if(currentValue.length != 0 && !currentFunc) {
+            currentFunc = func;
+            total = currentValue;
+            currentValue = "";
+        }
+        else
+            willError = true;
+
+        inputDisplay.innerText += (" " + symbol);
+    }
 }
 
 function equalsClickEvent() {
+    if(willError) {
+        clearValues();
+        inputDisplay.innerText = "ERROR";
+    }
     if(isNegative)
-    total = currentFunc(parseFloat(total), (parseFloat(currentValue) * -1));
+        total = currentFunc(parseFloat(total), (parseFloat(currentValue) * -1));
     else
-    total = currentFunc(parseFloat(total), parseFloat(currentValue));
+        total = currentFunc(parseFloat(total), parseFloat(currentValue));
 
     if(typeof total == 'number' && !Number.isInteger(total))
         total = total.toFixed(2);
@@ -73,15 +64,12 @@ function equalsClickEvent() {
     logdetails();
 }
 
-function calculate() {
-   
-}
-
 function clearValues() {
     total = 0;
     currentValue = "";
     currentFunc = null;
     inputDisplay.innerText = "";
+    willError = false;
     logdetails();
 }
 
@@ -90,5 +78,6 @@ function logdetails() {
     console.log(`total: ${total} (${typeof total})`)
     console.log(`currentValue: ${currentValue} (${typeof currentValue})`)
     console.log(`currentFunc: ${currentFunc}`)
+    console.log(`willError: ${willError}`);
 }
 
